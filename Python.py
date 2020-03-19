ce90e60ed1d084aedbde6e80ff1232f7f67ecde8
Sandeep Rawat <rawat.sandeep106@gmail.com>
2020-03-19 15:02:13 +0530

    Automation steps for github included

--- Python.py
+++ Python.py
@@ -1,6 +1,17 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:\\Users\\rawatsan\\Desktop\\My Python Projects\\chromedriver')
-driver.get('https://www.google.com/')
-driver.find_element_by_id('gb_70').click()
+path = 'C:\\Users\\rawatsan\\Desktop\\My Python Projects\\ChromeDriver\\chromedriver.exe'
+driver = webdriver.Chrome(path)
+
+driver.maximize_window()
+driver.delete_all_cookies()
+
+driver = webdriver.Chrome(path)
+driver.get('https://github.com/')
+driver.find_element_by_xpath('/html/body/div[1]/header/div/div[2]/div[2]/a[1]').click()
+driver.find_element_by_id('login_field').send_keys('rawatsandeep106')
+driver.find_element_by_id('password').send_keys('Tarzan@864')
+driver.find_element_by_xpath('//*[@id="login"]/form/div[4]/input[9]').click()
+driver.find_element_by_xpath('/html/body/div[1]/header/div[6]/details/summary').click()
+driver.get('https://github.com/new')
 
